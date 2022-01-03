75fd3667335578432d9519809817fe68270d87fc
Victor Fresno Gomez <63149516+vfg27@users.noreply.github.com>
2022-01-03 06:25:05 +0100

    Update bloque3.py

--- bloque3.py
+++ bloque3.py
@@ -4,14 +4,14 @@ import os
 import json
 import time
 from subprocess import call
-os.chdir("/root/bloque3/bookinfo/src/reviews")
+os.chdir("/root/bloque3.2/bookinfo/src/reviews")
 call(["pwd"])
 #call(["cd Ratings/practica_creativa2/bookinfo/src/reviews/reviews-wlpcfg"], shell=True)
 call(["docker run --rm -u root -v "+r'"'+"$(pwd)"+r'"'+":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build"], shell=True)
 os.chdir("/root")
 call(["pwd"])
 #call(["cd"], shell=True)
-os.chdir("/root/bloque3")
+os.chdir("/root/bloque3.2")
 call(["pwd"])
 #call(["cd bloque3"], shell=True)
 call(["docker-compose up"], shell=True)
