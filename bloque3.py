7211f6e89a702e2a986b41206bafbf9c63a549b4
Victor Fresno Gomez <63149516+vfg27@users.noreply.github.com>
2022-01-12 12:28:02 +0100

    Add files via upload

--- bloque3.py
+++ bloque3.py
@@ -4,16 +4,21 @@ import os
 import json
 import time
 from subprocess import call
-os.chdir("/root/bloque3.2/bookinfo/src/reviews")
+os.chdir("/root/bloque3.2/bookinfo/src/reviewsV1")
 call(["pwd"])
-#call(["cd Ratings/practica_creativa2/bookinfo/src/reviews/reviews-wlpcfg"], shell=True)
+#Ejecutamos el comando indicado
+call(["docker run --rm -u root -v "+r'"'+"$(pwd)"+r'"'+":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build"], shell=True)
+os.chdir("/root/bloque3.2/bookinfo/src/reviewsV2")
+call(["pwd"])
+#Ejecutamos el comando indicado
+call(["docker run --rm -u root -v "+r'"'+"$(pwd)"+r'"'+":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build"], shell=True)
+os.chdir("/root/bloque3.2/bookinfo/src/reviewsV3")
+call(["pwd"])
 #Ejecutamos el comando indicado
 call(["docker run --rm -u root -v "+r'"'+"$(pwd)"+r'"'+":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build"], shell=True)
 os.chdir("/root")
 call(["pwd"])
-#call(["cd"], shell=True)
 os.chdir("/root/bloque3.2")
 call(["pwd"])
-#call(["cd bloque3"], shell=True)
 #Arrancamos los contenedores
 call(["docker-compose up"], shell=True)
